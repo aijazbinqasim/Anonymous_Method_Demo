0df3ff7e0ac2632f7f5337bcf82408c369eff3a1
aijazbinqasim <aijazbinqasim@hotmail.com>
2024-03-11 19:48:24 +0500

    Add project files.

---
+++ Anonymous_Method_Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Anonymous_Method_Demo
+{
+    public delegate void Print1();
+    public delegate void Print2(string firstName);
+    public delegate void Addition(int val1);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Print1 p1 = delegate ()
+            {
+                Console.WriteLine("Inside p1 Anynomous method");
+            };
+
+            p1();
+
+
+            var firstName = "Aijaz";
+            Print2 p2 = delegate (string surname)
+            {
+                Console.WriteLine($"Full Name: {firstName} {surname}");
+            };
+
+            p2("Ali");
+
+           // HelperMethod(delegate (int val1) { Console.WriteLine($"Addition: {val1 + 100}"); });
+            HelperMethod(val1 =>  Console.WriteLine($"Addition: {val1 + 100}"));
+
+        }
+
+
+        public static void HelperMethod(Addition addition)
+        {
+            addition(100);
+        }
+    }
+}
